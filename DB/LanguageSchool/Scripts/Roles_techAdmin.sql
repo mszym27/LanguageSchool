9f6c402d98e4666c68fcbad9f8f6be9718b619e0
mszym27 <marcin.szymkowski2@poczta.onet.pl>
2018-11-05 20:14:26 +0100

    Dodanie skryptu z rolami oraz pierwszym użytkownikiem

---
+++ DB/LanguageSchool/Scripts/Roles_techAdmin.sql
@@ -0,0 +1,23 @@
+﻿INSERT INTO [Users].[Roles] ([Id],[Name]) VALUES (1 ,'Administrator')
+INSERT INTO [Users].[Roles] ([Id],[Name]) VALUES (2 ,'Sekretariat')
+INSERT INTO [Users].[Roles] ([Id],[Name]) VALUES (3 ,'Nauczyciel')
+INSERT INTO [Users].[Roles] ([Id],[Name]) VALUES (4 ,'Student')
+
+GO
+
+DECLARE @Now DATETIME = GETDATE()
+
+INSERT INTO [Users].[Users]
+    ([IsDeleted]
+    ,[CreationDate]
+    ,[DeletionDate]
+    ,[Login]
+    ,[Password]
+    ,[RoleId])
+VALUES
+    (0
+    ,@Now
+    ,NULL
+    ,'techAdmin'
+    ,'123' -- do zmiany przez uzytkownika
+    ,1)
