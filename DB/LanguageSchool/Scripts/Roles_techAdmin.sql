3c35c4e4f38d7dcb291196e812ad85c0581c5b6c
mszym27 <marcin.szymkowski2@poczta.onet.pl>
2018-12-02 11:02:34 +0100

    Zabezpieczenie przed wielokrotnym wdrożeniem skryptu z rolami

--- DB/LanguageSchool/Scripts/Roles_techAdmin.sql
+++ DB/LanguageSchool/Scripts/Roles_techAdmin.sql
@@ -1,23 +1,31 @@
-﻿INSERT INTO [Users].[Roles] ([Id],[Name]) VALUES (1 ,'Administrator')
-INSERT INTO [Users].[Roles] ([Id],[Name]) VALUES (2 ,'Sekretariat')
-INSERT INTO [Users].[Roles] ([Id],[Name]) VALUES (3 ,'Nauczyciel')
-INSERT INTO [Users].[Roles] ([Id],[Name]) VALUES (4 ,'Student')
+﻿IF NOT EXISTS(SELECT 1 FROM [Users].[Roles] WHERE Id = 1)
+	INSERT INTO [Users].[Roles] ([Id],[Name]) VALUES (1 ,'Administrator')
+
+IF NOT EXISTS(SELECT 1 FROM [Users].[Roles] WHERE Id = 2)
+	INSERT INTO [Users].[Roles] ([Id],[Name]) VALUES (2 ,'Sekretariat')
+
+IF NOT EXISTS(SELECT 1 FROM [Users].[Roles] WHERE Id = 3)
+	INSERT INTO [Users].[Roles] ([Id],[Name]) VALUES (3 ,'Nauczyciel')
+
+IF NOT EXISTS(SELECT 1 FROM [Users].[Roles] WHERE Id = 4)
+	INSERT INTO [Users].[Roles] ([Id],[Name]) VALUES (4 ,'Student')
 
 GO
 
 DECLARE @Now DATETIME = GETDATE()
 
-INSERT INTO [Users].[Users]
-    ([IsDeleted]
-    ,[CreationDate]
-    ,[DeletionDate]
-    ,[Login]
-    ,[Password]
-    ,[RoleId])
-VALUES
-    (0
-    ,@Now
-    ,NULL
-    ,'techAdmin'
-    ,'123' -- do zmiany przez uzytkownika
-    ,1)
+IF NOT EXISTS(SELECT 1 FROM [Users].[Users] WHERE RoleId = 1)
+	INSERT INTO [Users].[Users]
+		([IsDeleted]
+		,[CreationDate]
+		,[DeletionDate]
+		,[Login]
+		,[Password]
+		,[RoleId])
+	VALUES
+		(0
+		,@Now
+		,NULL
+		,'techAdmin'
+		,'123' -- do zmiany przez uzytkownika
+		,1)
