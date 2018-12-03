8a9c2e8faf5384d6703ded31b7a7705d3b5e87df
marcinszymko <marcinszymko@201402020H>
2018-12-03 17:24:56 +0100

    Ustawienie strony domowej na kursy

--- LanguageSchool/App_Start/RouteConfig.cs
+++ LanguageSchool/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace LanguageSchool
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Course", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
