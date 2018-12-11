aa60fa9e59f449273aaeb16ddea3168c2bd44beb
mszym27 <marcin.szymkowski2@poczta.onet.pl>
2018-12-11 23:30:58 +0100

    Dalsze zmiany dla ról

--- DB/LanguageSchool/Scripts/Roles_techAdmin.sql
+++ DB/LanguageSchool/Scripts/Roles_techAdmin.sql
@@ -2,10 +2,10 @@
 	INSERT INTO [Users].[Roles] ([Id],[Name]) VALUES (1 ,'Administrator')
 
 IF NOT EXISTS(SELECT 1 FROM [Users].[Roles] WHERE Id = 2)
-	INSERT INTO [Users].[Roles] ([Id],[Name]) VALUES (2 ,'Sekretariat')
+	INSERT INTO [Users].[Roles] ([Id],[Name]) VALUES (2 ,'Secretary')
 
 IF NOT EXISTS(SELECT 1 FROM [Users].[Roles] WHERE Id = 3)
-	INSERT INTO [Users].[Roles] ([Id],[Name]) VALUES (3 ,'Nauczyciel')
+	INSERT INTO [Users].[Roles] ([Id],[Name]) VALUES (3 ,'Teacher')
 
 IF NOT EXISTS(SELECT 1 FROM [Users].[Roles] WHERE Id = 4)
 	INSERT INTO [Users].[Roles] ([Id],[Name]) VALUES (4 ,'Student')
@@ -26,6 +26,54 @@ IF NOT EXISTS(SELECT 1 FROM [Users].[Users] WHERE RoleId = 1)
 		(0
 		,@Now
 		,NULL
-		,'techAdmin'
-		,'123' -- do zmiany przez uzytkownika
+		,'BL\techAdmin'
+		,'123'
 		,1)
+
+IF NOT EXISTS(SELECT 1 FROM [Users].[Users] WHERE RoleId = 2)
+	INSERT INTO [Users].[Users]
+		([IsDeleted]
+		,[CreationDate]
+		,[DeletionDate]
+		,[Login]
+		,[Password]
+		,[RoleId])
+	VALUES
+		(0
+		,@Now
+		,NULL
+		,'BL\S_DL'
+		,'456'
+		,2)
+
+IF NOT EXISTS(SELECT 1 FROM [Users].[Users] WHERE RoleId = 3)
+	INSERT INTO [Users].[Users]
+		([IsDeleted]
+		,[CreationDate]
+		,[DeletionDate]
+		,[Login]
+		,[Password]
+		,[RoleId])
+	VALUES
+		(0
+		,@Now
+		,NULL
+		,'BL\T_LL_0001'
+		,'789'
+		,3)
+
+IF NOT EXISTS(SELECT 1 FROM [Users].[Users] WHERE RoleId = 4)
+	INSERT INTO [Users].[Users]
+		([IsDeleted]
+		,[CreationDate]
+		,[DeletionDate]
+		,[Login]
+		,[Password]
+		,[RoleId])
+	VALUES
+		(0
+		,@Now
+		,NULL
+		,'BL\S_MS_0001'
+		,'012'
+		,4)
--- LanguageSchool/Consts.cs
+++ LanguageSchool/Consts.cs
@@ -17,10 +17,33 @@ namespace LanguageSchool
         public static List<MenuViewModel> menus = (
             new List<MenuViewModel>()
             {
+                // Administrator
                 new MenuViewModel { ShownName = "Lista kursów", ControllerName = "Course", ActionName = "Index", RoleName = "Administrator" },
                 new MenuViewModel { ShownName = "Testy", ControllerName = "Test", ActionName = "Index", RoleName = "Administrator" },
-                new MenuViewModel { ShownName = "Dane użytkowników", ControllerName = "Users", ActionName = "Index", RoleName = "Administrator" },
-                new MenuViewModel { ShownName = "Komunikaty", ControllerName = "UserData", ActionName = "Index", RoleName = "Administrator" }
+                new MenuViewModel { ShownName = "Materiały", ControllerName = "Test", ActionName = "Index", RoleName = "Administrator" },
+                new MenuViewModel { ShownName = "Dane kontaktowe", ControllerName = "Users", ActionName = "Index", RoleName = "Administrator" },
+                new MenuViewModel { ShownName = "Komunikaty", ControllerName = "UserData", ActionName = "Index", RoleName = "Administrator" },
+                new MenuViewModel { ShownName = "Zapisy", ControllerName = "UserData", ActionName = "Index", RoleName = "Secretary" },
+
+                //Sekretariat
+                new MenuViewModel { ShownName = "Lista kursów", ControllerName = "Course", ActionName = "Index", RoleName = "Secretary" },
+                new MenuViewModel { ShownName = "Dane użytkowników", ControllerName = "Users", ActionName = "Index", RoleName = "Secretary" },
+                new MenuViewModel { ShownName = "Komunikaty", ControllerName = "Test", ActionName = "Index", RoleName = "Secretary" },
+                new MenuViewModel { ShownName = "Zapisy", ControllerName = "UserData", ActionName = "Index", RoleName = "Secretary" },
+
+                //Nauczyciel
+                new MenuViewModel { ShownName = "Moje kursy", ControllerName = "Course", ActionName = "Index", RoleName = "Teacher" },
+                new MenuViewModel { ShownName = "Moje materiały", ControllerName = "Test", ActionName = "Index", RoleName = "Teacher" },
+                new MenuViewModel { ShownName = "Moje testy", ControllerName = "Test", ActionName = "Index", RoleName = "Teacher" },
+                new MenuViewModel { ShownName = "Moje komunikaty", ControllerName = "Test", ActionName = "Index", RoleName = "Teacher" },
+                new MenuViewModel { ShownName = "Uczniowie", ControllerName = "Users", ActionName = "Index", RoleName = "Teacher" },
+
+                //Uczeń
+                new MenuViewModel { ShownName = "Moje kursy", ControllerName = "Course", ActionName = "Index", RoleName = "Student" },
+                new MenuViewModel { ShownName = "Moje materiały", ControllerName = "Test", ActionName = "Index", RoleName = "Student" },
+                new MenuViewModel { ShownName = "Moje testy", ControllerName = "Test", ActionName = "Index", RoleName = "Student" },
+                new MenuViewModel { ShownName = "Moje komunikaty", ControllerName = "Test", ActionName = "Index", RoleName = "Student" },
+                new MenuViewModel { ShownName = "Prowadzący", ControllerName = "Users", ActionName = "Index", RoleName = "Student" },
             }
         );
     }
