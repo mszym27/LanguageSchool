5c95e3b9a3af593683b48629dc64c46fd5d88d38
marcinszymko <marcinszymko@201402020H>
2019-01-04 10:59:24 +0100

    Podpięcie metody tworzącej pod drugą z akcji

--- LanguageSchool/Controllers/UserDataController.cs
+++ LanguageSchool/Controllers/UserDataController.cs
@@ -163,6 +163,7 @@ namespace LanguageSchool.Controllers
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
+        [Route("UserData/Create")]
         [Route("UserData/Create/{Id}")]
         [ValidateAntiForgeryToken]
         [Authorize(Roles = "Secretary, Administrator")]
