54b60882c29dc5f08950459c85f62e4138fc3f2d
mszym27 <marcin.szymkowski2@poczta.onet.pl>
2019-01-21 22:51:39 +0100

    Dodanie informacji o obecności otwartych odpowiedzi w wypadku użytkownika

--- LanguageSchool/Models/ViewModels/UserViewModel.cs
+++ LanguageSchool/Models/ViewModels/UserViewModel.cs
@@ -12,6 +12,7 @@ namespace LanguageSchool.Models.ViewModels
         public string EmailAdress { get; set; }
         public string PublicPhoneNumber { get; set; }
         public bool IsMarked { get; set; }
+        public List<UsersTests> UserTests { get; set; }
 
         public UserViewModel() { }
 
@@ -20,6 +21,7 @@ namespace LanguageSchool.Models.ViewModels
             Id = user.Id;
             CreationDate = user.CreationDate.ToString("yyyy/MM/dd");
             UserGroups = user.UsersGroups.ToList();
+            UserTests = user.UsersTests.ToList();
 
             var userData = user.UserData.OfType<UserData>().FirstOrDefault();
             Fullname = userData.Name + " " + userData.Surname + " (" + user.Login + ")";
