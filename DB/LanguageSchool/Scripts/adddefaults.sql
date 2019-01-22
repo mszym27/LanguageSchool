feef794c4fb3df0788ec1ba67abc2634757d6781
mszym27 <marcin.szymkowski2@poczta.onet.pl>
2019-01-22 10:22:43 +0100

    Poprawki przed oddaniem

--- DB/LanguageSchool/Scripts/adddefaults.sql
+++
@@ -1,7 +0,0 @@
-﻿SELECT 'ALTER TABLE ' + SCHEMA_NAME(schema_id) + '.' + t.name +
-	' ADD CONSTRAINT DF_' + t.name + 
-	'CreationDate DEFAULT GETDATE() FOR CreationDate'
-FROM sys.tables AS t
-INNER JOIN sys.columns c 
-	ON t.OBJECT_ID = c.OBJECT_ID
-WHERE c.name LIKE 'CreationDate'
--- DB/LanguageSchool/Scripts/clear_groups.sql
+++
@@ -1,5 +0,0 @@
-﻿DELETE FROM [Materials].[Materials]
-DELETE FROM [Courses].[LessonSubjects]
-DELETE FROM [Courses].[GroupTimes]
-DELETE FROM [Users].[UsersGroups]
-DELETE FROM [Courses].[Groups]
