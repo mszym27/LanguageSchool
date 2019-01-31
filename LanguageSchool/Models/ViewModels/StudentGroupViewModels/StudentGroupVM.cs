9d9e4a8537260ae17b695ee1417ea397f3a1ff7d
marcinszymko <marcinszymko@201402020H>
2019-01-31 17:29:17 +0100

    Modyfikacje w wyświetlaniu wartości z podglądu studenta

--- LanguageSchool/Models/ViewModels/StudentGroupViewModels/StudentGroupVM.cs
+++ LanguageSchool/Models/ViewModels/StudentGroupViewModels/StudentGroupVM.cs
@@ -8,7 +8,11 @@ namespace LanguageSchool.Models.ViewModels.StudentGroupViewModels
 {
     public class StudentGroupVM
     {
+        public int StudentId { get; }
         public string StudentName { get; }
+        public string EnrollmentDate { get; }
+        public string EmailAdress { get; }
+        public string PhoneNumber { get; }
 
         public List<UserTestViewModel> TakenTests { get; }
         public List<StudentAwaitingTestVM> AwaitingMark { get; }
@@ -18,9 +22,14 @@ namespace LanguageSchool.Models.ViewModels.StudentGroupViewModels
             var group = userGroup.Group;
             var student = userGroup.User;
 
+            EnrollmentDate = userGroup.CreationDate.ToString("yyyy/MM/dd");
+
             var studentData = userGroup.User.UserData.First();
 
+            StudentId = student.Id;
             StudentName = studentData.Name + " " + studentData.Surname;
+            EmailAdress = studentData.EmailAdress;
+            PhoneNumber = studentData.PublicPhoneNumber;
 
             var studentTests = student.UsersTests.Where(t => t.GroupId == group.Id).ToList();
 
