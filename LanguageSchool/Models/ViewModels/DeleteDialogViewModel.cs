8fa0d648873f3606dcc75c83ed9e437f561bafac
mszym27 <marcin.szymkowski2@poczta.onet.pl>
2019-02-02 00:57:12 +0100

    Opracowanie metody na usuwanie itemów z list, dodanie przycisku do listy materiałów na temacie

--- LanguageSchool/Models/ViewModels/DeleteDialogViewModel.cs
+++ LanguageSchool/Models/ViewModels/DeleteDialogViewModel.cs
@@ -8,15 +8,26 @@ namespace LanguageSchool.Models.ViewModels
 {
     public class DeleteDialogViewModel
     {
-        public int Id { get; set; }
-        public string Dialog { get; set; }
-        public string ControllerName { get; set; }
+        public int Id { get; }
+        public string Dialog { get; }
+        public string ControllerName { get; }
+        public string ModalId { get; }
 
         public DeleteDialogViewModel(int Id, string ControllerName, string Dialog)
         {
             this.Id = Id;
             this.Dialog = Dialog;
             this.ControllerName = ControllerName;
+
+            this.ModalId = "deleteDialog";
+        }
+
+        public DeleteDialogViewModel(int Id, string ControllerName, string Dialog, string ModalId)
+        {
+            this.Id = Id;
+            this.Dialog = Dialog;
+            this.ControllerName = ControllerName;
+            this.ModalId = ModalId;
         }
     }
 }
