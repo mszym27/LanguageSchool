8a2465d53bdc101e963dad20ef0a4d54e25eb4b5
mszym27 <marcin.szymkowski2@poczta.onet.pl>
2019-02-02 04:06:06 +0100

    Dodanie skryptów do ew. zmian na bazie

---
+++ DB/LanguageSchool/Scripts/adddefaults.sql
@@ -0,0 +1,7 @@
+﻿SELECT 'ALTER TABLE ' + SCHEMA_NAME(schema_id) + '.' + t.name +
+	' ADD CONSTRAINT DF_' + t.name + 
+	'CreationDate DEFAULT GETDATE() FOR CreationDate'
+FROM sys.tables AS t
+INNER JOIN sys.columns c 
+	ON t.OBJECT_ID = c.OBJECT_ID
+WHERE c.name LIKE 'CreationDate'
