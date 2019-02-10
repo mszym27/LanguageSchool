4fd6e49dfde7e7c777eb79de5d235e9d2dbb78c4
mszym27 <marcin.szymkowski2@poczta.onet.pl>
2019-02-10 17:01:07 +0100

    Zmiana terminu w skrypcie

--- DB/LanguageSchool/Scripts/skrypt_aktualizacja.sql
+++ DB/LanguageSchool/Scripts/skrypt_aktualizacja.sql
@@ -1,5 +1,5 @@
 ﻿DECLARE @now DATE = GETDATE()
 
-UPDATE [Courses].[Courses] SET IsActive = 0 WHERE [StartDate] = @now
+UPDATE [Courses].[Courses] SET IsActive = 0 WHERE [StartDate] = DATEADD(DAY, 7, @now)
 
 DELETE FROM [Materials].[Materials] WHERE CAST([DeletionDate] AS DATE) = DATEADD(DAY, -31, @now)
